be0bf33494d6df7c167d245b0dccde443d8475c9
Professional-Miles <90847818+Professional-Miles@users.noreply.github.com>
2021-09-28 15:44:30 +0100

    Added object exception handling with 2 classes.

--- src/com/sparta/exceptionse/ExceptionsDriver.java
+++ src/com/sparta/exceptionse/ExceptionsDriver.java
@@ -6,21 +6,29 @@ import java.util.Date;
 
 public class ExceptionsDriver {
 
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 0;
-        System.err.println("Hello");
+    //public static void main(String[] args) throws ParseException
 
-        DateFormat df = DateFormat.getDateInstance();
+    public static void main(String[] args) {
+
+//        try {
+//            int c = a/b;
+//            System.out.println(c);
+//            System.out.println(df.parse("2021-09-28"));
+//        } catch(ParseException pe){
+//            System.err.println(pe.getMessage());
+//        } catch (ArithmeticException ae){
+//            System.err.println("Arithmetic Exception");
+//        } // catch (ParseException | ArithmeticException)
+
+        ExceptionsWorker worker = new ExceptionsWorker();
         try {
-            int c = a/b;
-            System.out.println(c);
-            System.out.println(df.parse("2021-09-28"));
-        } catch(ParseException pe){
-            System.err.println(pe.getMessage());
-        } catch (ArithmeticException ae){
-            System.err.println("Arithmetic Exception");
+            worker.myFunc();
+        } catch (ArithmeticException e) {
+            //never leave catch block empty
+            //never print to system.out
+            e.printStackTrace();
         }
+
     }
 
 }
--- src/com/sparta/exceptionse/ExceptionsWorker.java
+++ src/com/sparta/exceptionse/ExceptionsWorker.java
@@ -1,4 +1,20 @@
 package com.sparta.exceptionse;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+
 public class ExceptionsWorker {
+
+    public void myFunc() throws ArithmeticException{
+
+        int a = 10;
+        int b = 0;
+        System.err.println("Hello");
+
+        DateFormat df = DateFormat.getDateInstance();
+        int c = a/b;
+        System.out.println(c);
+
+    }
+
 }
