939b73736c1636a9e326631b466dd5071b911e25
Professional-Miles <90847818+Professional-Miles@users.noreply.github.com>
2021-09-28 15:25:20 +0100

    Added some exception examples.

--- src/com/sparta/exceptionse/ExceptionsDriver.java
+++ src/com/sparta/exceptionse/ExceptionsDriver.java
@@ -1,13 +1,23 @@
 package com.sparta.exceptionse;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
+
 public class ExceptionsDriver {
 
     public static void main(String[] args) {
         int a = 10;
         int b = 0;
-        int c = a/b;
-
+        System.err.println("Hello");
+        int c = 0; //a/b;
         System.out.println(c);
+        DateFormat df = DateFormat.getDateInstance();
+        try {
+            System.out.println(df.parse("2021-09-28"));
+        } catch(ParseException pe){
+            System.err.println(pe.getMessage());
+        }
     }
 
 }
